17b0d83cdadfbb06a2ea650e770a027ca222fb64
Esther03u <150509740+Esther03u@users.noreply.github.com>
2026-07-15 03:00:39 +0700

    Increase logo size in menu

--- src/components/StaggeredMenu/StaggeredMenu.jsx
+++ src/components/StaggeredMenu/StaggeredMenu.jsx
@@ -389,7 +389,7 @@ export const StaggeredMenu = ({
             alt="Logo"
             className="sm-logo-img"
             draggable={false}
-            style={{ height: '40px', width: 'auto', objectFit: 'contain' }}
+            style={{ height: '64px', width: 'auto', objectFit: 'contain' }}
           />
         </div>
         <button
